c7cfc9409095e70f87f93121cc0318672815ecc4
Levente Holló <levente.hollo@nokia.com>
2017-01-30 10:59:29 +0100

    new roles: development, look-and-feel, sublime-text, office

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-eOS-ansible
+How to use:
+
+ansible-playbook install_elementary_os.yml -i hosts --ask-become-pass -vvv
+
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt update
+#sudo apt install ansible
 
 #sudo ansible-galaxy install cmprescott.chrome
+ansible-galaxy install igor_mukhin.sublimetext3_packagecontrol
+ansible-galaxy install ChristopherDavenport.universal-java
+ansible-galaxy install gantsign.maven
+ansible-galaxy install gantsign.intellij
+ansible-galaxy install tersmitten.pycharm
+ansible-galaxy install geerlingguy.nodejs
